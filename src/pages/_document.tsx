e7c3bfc8b8b169842ba1a0d72f5692f004738aee
dough <viktor.melin@me.com>
2023-04-19 16:59:44 +0200

    feat(theme): mantine setup

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,16 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { emotionCache } from '@/emotion-cache';
+import { ServerStyles, createGetInitialProps, createStylesServer } from '@mantine/next';
+import Document, { Html, Head, Main, NextScript, DocumentContext } from 'next/document';
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+const stylesServer = createStylesServer(emotionCache);
+const getInitialProps = createGetInitialProps();
+
+export default class _Document extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return {
+      ...initialProps,
+      styles: [initialProps.styles, <ServerStyles html={initialProps.html} server={stylesServer} key='styles' />],
+    };
+  }
 }
